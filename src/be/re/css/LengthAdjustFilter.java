c6a2d348cebd3de6dd150a7131ff539db82f45bc
Gerke Geurts <ggeurts@geurtsrus.com>
2014-12-11 17:46:56 +0100

    Rewrite of integer test to avoid exceptions

--- src/be/re/css/LengthAdjustFilter.java
+++ src/be/re/css/LengthAdjustFilter.java
@@ -10,146 +10,112 @@ import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.AttributesImpl;
 import org.xml.sax.helpers.XMLFilterImpl;
 
-
-
 /**
  * Length properties without a unit are given the unit "px".
+ *
  * @author Werner Donn\u00e9
  */
-
 class LengthAdjustFilter extends XMLFilterImpl
-
 {
-
-  private static final Set<String> ofLengthType = new HashSet<>(Arrays.asList(new String[]
-        {
-          "background-position",
-          "border-bottom-width",
-          "border-left-width",
-          "border-right-width",
-          "border-spacing",
-          "border-top-width",
-          "bottom",
-          "font-size",
-          "height",
-          "img-height",
-          "img-width",
-          "left",
-          "letter-spacing",
-          "line-height",
-          "margin-bottom",
-          "margin-left",
-          "margin-right",
-          "margin-top",
-          "marker-offset",
-          "max-height",
-          "max-width",
-          "min-height",
-          "min-width",
-          "outline-width",
-          "padding-bottom",
-          "padding-left",
-          "padding-right",
-          "padding-top",
-          "right",
-          "size",
-          "text-indent",
-          "text-shadow",
-          "top",
-          "vertical-align",
-          "width",
-          "word-spacing"
-        }));
-
-
-  LengthAdjustFilter()
-  {
-  }
-
-
-
-  LengthAdjustFilter(XMLReader parent)
-  {
-    super(parent);
-  }
-
-
-
-  private static Attributes
-  adjustAttributes(Attributes atts)
-  {
-    AttributesImpl	result = null;
-
-    for (int i = 0; i < atts.getLength(); ++i)
+    private static final Set<String> ofLengthType = new HashSet<>(Arrays.asList(new String[]
     {
-      if
-      (
-        Constants.CSS.equals(atts.getURI(i))			&&
-        ofLengthType.contains(atts.getLocalName(i))
-      )
-      {
-        boolean		changed = false;
-        String		newValue = "";
-        StringTokenizer	tokenizer = new StringTokenizer(atts.getValue(i), " ");
+        "background-position",
+        "border-bottom-width",
+        "border-left-width",
+        "border-right-width",
+        "border-spacing",
+        "border-top-width",
+        "bottom",
+        "font-size",
+        "height",
+        "img-height",
+        "img-width",
+        "left",
+        "letter-spacing",
+        "line-height",
+        "margin-bottom",
+        "margin-left",
+        "margin-right",
+        "margin-top",
+        "marker-offset",
+        "max-height",
+        "max-width",
+        "min-height",
+        "min-width",
+        "outline-width",
+        "padding-bottom",
+        "padding-left",
+        "padding-right",
+        "padding-top",
+        "right",
+        "size",
+        "text-indent",
+        "text-shadow",
+        "top",
+        "vertical-align",
+        "width",
+        "word-spacing"
+    }));
 
-        while (tokenizer.hasMoreTokens())
+    LengthAdjustFilter()
+    {
+    }
+
+    LengthAdjustFilter(XMLReader parent)
+    {
+        super(parent);
+    }
+
+    private static Attributes adjustAttributes(Attributes atts)
+    {
+        AttributesImpl result = (AttributesImpl)atts;
+
+        for (int i = 0; i < atts.getLength(); ++i)
         {
-          String	token = tokenizer.nextToken();
+            if (Constants.CSS.equals(atts.getURI(i)) && ofLengthType.contains(atts.getLocalName(i)))
+            {
+                boolean changed = false;
+                String newValue = "";
+                StringTokenizer tokenizer = new StringTokenizer(atts.getValue(i), " ");
 
-          if (mustReplace(token))
-          {
-            changed = true;
-            newValue += (newValue.equals("") ? "" : " " ) + token + "px";
-          }
-          else
-          {
-            newValue += (newValue.equals("") ? "" : " " ) + token;
-          }
+                while (tokenizer.hasMoreTokens())
+                {
+                    String token = tokenizer.nextToken();
+
+                    if (mustReplace(token))
+                    {
+                        changed = true;
+                        newValue += (newValue.equals("") ? "" : " ") + token + "px";
+                    }
+                    else
+                    {
+                        newValue += (newValue.equals("") ? "" : " ") + token;
+                    }
+                }
+
+                if (changed)
+                {
+                    result.setValue(i, newValue);
+                }
+            }
         }
 
-        if (changed)
+        return result;
+    }
+
+    private static boolean mustReplace(String s)
+    {
+        if (s == null) return false;
+        for (int i = 0; i < s.length(); i++)
         {
-          if (result == null)
-          {
-            result = new AttributesImpl(atts);
-          }
-
-          result.setValue(i, newValue);
+            if (!Character.isDigit(s.charAt(i))) return false;
         }
-      }
+        return true;
     }
 
-    return result != null ? (Attributes) result : atts;
-  }
-
-
-
-  private static boolean
-  mustReplace(String s)
-  {
-    try
+    @Override
+    public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException
     {
-      return Integer.parseInt(s) > 0;
+        super.startElement(namespaceURI, localName, qName, adjustAttributes(atts));
     }
-
-    catch (NumberFormatException e)
-    {
-      return false;
-    }
-  }
-
-
-
-  public void
-  startElement
-  (
-    String	namespaceURI,
-    String	localName,
-    String	qName,
-    Attributes	atts
-  ) throws SAXException
-  {
-    super.startElement(namespaceURI, localName, qName, adjustAttributes(atts));
-  }
-
 } // LengthAdjustFilter
